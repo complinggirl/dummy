7156064767cc6650647bd840fb258f306c5ee1fa
vcolavin <vcolavin@gmail.com>
2015-09-20 18:34:33 -0700

    some refactoring

--- dummy.js
+++ dummy.js
@@ -1,103 +1,78 @@
-
-
 $(document).ready(function() {
 
-var myGuess;
-var mystery;
-var tries=0;
-var myRand=Math.floor((Math.random() * 100) + 1);
+  var myGuess;
+  var mystery;
+  var tries = 0;
+  var myRand = Math.floor((Math.random() * 100) + 1);
 
 
 
-function loopy(myTry){
-  alert(myTry);
-  alert(myRand);
-  tries +=1;
-  if (myTry == myRand){
-    alert("hey, you win");
-    window.location.reload();
-  }
-  else if (myTry < myRand){
-      if(tries===7){
-      alert("you ran out of tries, you lose");
+  function loopy(myTry){
+
+    tries += 1;
+
+    if (myTry == myRand){
+      alert("hey, you win");
       window.location.reload();
+    }
+    else if (myTry < myRand){
+      if(tries === 7){
+        alert("you ran out of tries, you lose");
+        window.location.reload();
       }
       else{
-        alert("That's too low, try again. You have "+(7-tries) +" tries left")
-      };
-  }
-  else{
-    if(tries===7){
-    alert("you ran out of tries, you lose");
-    window.location.reload();
+        alert("That's too low, try again. You have "+(7 - tries) +" tries left");
+      }
     }
     else{
-      alert("That's too high, try again. You have "+(7-tries) +" tries left")
+      if(tries === 7) {
+        alert("you ran out of tries, you lose");
+
+        // avoid window reload
+        window.location.reload();
+
+        // reset data "manually"
+        // make the data reset a separate function
       }
-    };
-
-};
-
-
-for(i = 1; i <=25; i++) {
-      var $something= $('<input/>').attr({ type: 'button', class: "guess", id:'btn'+i, value:i});
-      $("#one").append($something);
-    };
-      $('#one').delegate('input[type=button]','click',function(){
-      //  alert(this.value);
-      //  $(this).css("background", "red");
-      //  $(this).attr('disabled', true);
-      });
+      else {
+        alert("That's too high, try again. You have "+ (7 - tries) +" tries left");
+      }
+    }
+  }
 
 
+  // append new element to #one, then #two, etc.
+  var divNumber = "one";
+  for(i = 1; i <= 100; i++) {
+    var $something = $('<input/>').attr({ type: 'button', class: "guess", id:'btn'+i, value:i});
 
-//setting up second tow
-for(i = 26; i <=50; i++) {
-          var $something= $('<input/>').attr({ type: 'button', class: "guess", id:'btn'+i, value:i});
-          $("#two").append($something);
-        };
-          $('#two').delegate('input[type=button]','click',function(){
-        //  alert(this.value);
-        //  $(this).css("background", "red");
-        //  $(this).attr('disabled', true);
-          });
+    if (i > 25) {
+      divNumber = "two";
+    }
 
+    if (i > 50) {
+      divNumber = "three";
+    }
 
-//setting up third row
-for(i = 51; i <=75; i++) {
-            var $something= $('<input/>').attr({ type: 'button', class: "guess", id:'btn'+i, value:i});
-            $("#three").append($something);
-          };
-            $('#three').delegate('input[type=button]','click',function(){
-          //  alert(this.value);
-          //  $(this).css("background", "red");
-          //  $(this).attr('disabled', true);
+    if (i > 75) {
+      divNumber = "four";
+    }
 
-          });
+    $("#" + divNumber).append($something);
+  }
 
-
-
-for(i = 76; i <=100; i++){
-              var $something= $('<input/>').attr({ type: 'button', class: "guess", id:'btn'+i, value:i});
-              $("#four").append($something);
-            };
-              $('#four').delegate('input[type=button]','click',function(){
-            //  alert(this.value);
-            //  $(this).css("background", "red");
-            //  $(this).attr('disabled', true);
-           });
-
-
-
-
-           $(document).on('click', ".guess", function(){
-                      loopy(this.value);
-                       $(this).css("background", "red");
-                       $(this).attr('disabled', true);
-
-                    });
-
-
-
+  $(document).on('click', ".guess", function(){
+    loopy(this.value);
+    $(this).css("background", "red");
+    $(this).attr('disabled', true);
+  });
 
 });
+
+
+// create all elements
+// define listener
+// apply listener to those elements by adding a class
+
+// how to avoid alert box
+// change a notification div's text instead of alert
